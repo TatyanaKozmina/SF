63ad43331d6925b2ec77937f389b5eb5b848f612
TatyanaKozmina <tatyana_vinogradova@hotmail.com>
2022-05-17 16:11:34 +0300

    Task5.5 added

---
+++ Task5.5/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите фразу");
+string phrase = Console.ReadLine();
+
+Console.WriteLine("Введите глубину эха");
+int echodeep = int.Parse(Console.ReadLine());
+
+Echo(phrase, echodeep);
+
+void Echo(string source, int deep)
+{
+    Console.WriteLine(source);
+    if(deep > 1 && source.Length > 2)
+    {
+        deep--;
+        source = source.Remove(0, 2);
+        Echo(source, deep);
+    }
+}
