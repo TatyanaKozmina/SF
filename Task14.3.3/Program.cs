804cf4e036dc688317a19965e8145e5a17be5622
Tatyana vinogradova <tatyana_vinogradova@hotmail.com>
2022-06-13 08:37:19 +0300

    14.3.3. ПРАКТИКА (HW-03)

---
+++ Task14.3.3/Program.cs
@@ -0,0 +1,14 @@
+﻿//  создаём пустой список с типом данных Contact
+var phoneBook = new List<Contact>();
+
+// добавляем контакты
+phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+// выводим контакты, отсортированные сперва по имени, а затем по фамилии
+foreach (var contact in phoneBook.OrderBy(c => c.Name).ThenBy(c => c.LastName))
+    Console.WriteLine(contact.Name + ", " + contact.LastName);
