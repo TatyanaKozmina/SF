8e039b4ed7ecabbaaf624d54a20ae123382d30e9
Tatyana vinogradova <tatyana_vinogradova@hotmail.com>
2022-06-14 14:28:14 +0300

    15.6.6. ПРАКТИЧЕСКОЕ ЗАДАНИЕ (HW-03)

---
+++ Task15.6.6/Program.cs
@@ -0,0 +1,21 @@
+﻿var classes = new[]
+{
+    new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+    new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+    new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+};
+
+var allStudents = GetAllStudents(classes);
+
+Console.WriteLine(string.Join(" ", allStudents));
+
+static string[] GetAllStudents(Classroom[] classes)
+{
+    var res = classes.Select(s => s.Students.Select(s => s)).Aggregate((x, y) => x.Concat(y));
+    return res.ToArray();
+}
+
+class Classroom
+{
+    public List<string> Students = new List<string>();
+}
