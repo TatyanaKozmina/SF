4acb93daa1420a9046996936d0cf212739bbc642
Tatyana vinogradova <tatyana_vinogradova@hotmail.com>
2022-06-20 11:41:07 +0300

    Начальное состояние проекта.

---
+++ SocialNetwork/DAL/Repositories/IFriendRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialNetwork.DAL.Repositories
+{
+    public interface IFriendRepository
+    {
+        int Create(FriendEntity friendEntity);
+        IEnumerable<FriendEntity> FindAllByUserId(int userId);
+        int Delete(int id);
+    }
+}
---
+++ SocialNetwork/DAL/Repositories/IMessageRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialNetwork.DAL.Repositories
+{
+    public interface IMessageRepository
+    {
+        int Create(MessageEntity messageEntity);
+        IEnumerable<MessageEntity> FindBySenderId(int senderId);
+        IEnumerable<MessageEntity> FindByRecipientId(int recipientId);
+        int DeleteById(int messageId);
+    }
+}
