53b25e3f947c0936d0640bc5157497267b211ec9
Tatyana vinogradova <tatyana_vinogradova@hotmail.com>
2022-07-13 10:31:47 +0300

    Task 32.11 Добавлен вывод из таблицы базы данных в представление

---
+++ MVCStartApp/Controllers/HistoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCStartApp.Models;
+using MVCStartApp.Models.DB;
+
+namespace MVCStartApp.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly IHistoryRepository _repo;
+
+        public HistoryController(IHistoryRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var historyRecords = await _repo.GetHistory();
+            return View(historyRecords);
+        }
+    }
+}
